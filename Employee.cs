ea77e9b019bc6c5f5e7a79057756fac19c1db8fe
adel-filo <69355398+adel-filo@users.noreply.github.com>
2020-09-09 15:54:43 +0200

    Creating Serialize & Deserialize

---
+++ Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializeDeserialize
+{
+    class Employee
+    {
+        public string FirstName;
+        public string LastName;
+        public int employeeID;
+        public string Designation;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializeDeserialize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            string JSONresult = JsonConvert.SerializeObject(emp);
+            string path = @"C:\Users\Adel\Desktop\.Net\SerializeDeserialize\Employee.json";
+
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+                using (var tw = new StreamWriter(path, true))
+                {
+                    tw.WriteLine(JSONresult.ToString());
+                    tw.Close();
+                }
+
+            }
+            else if (!File.Exists(path))
+            {
+                using (var tw = new StreamWriter(path, true))
+                {
+                    tw.WriteLine(JSONresult.ToString());
+                    tw.Close();
+                }
+            }
+        }
+    }
+}
